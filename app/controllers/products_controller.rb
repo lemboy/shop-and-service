679667166be898db2e0dc38aa7f6e84246221043
lemboy <lemboy@gmail.com>
2015-01-29 12:35:26 +0300

    Add product new/update abilities

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -6,8 +6,45 @@ class ProductsController < ApplicationController
     @product_parents = Product.where("id in (?)", parents_list_id(grp_id)) unless grp_id.nil?
   end
   
-  def edit
-    
+  def show
+    @product = Product.find(params[:id])
   end
+  
+  def edit
+    @product = Product.find(params[:id])
+  end
+  
+  def update
+    @product = Product.find(params[:id])
+    if @product.update_attributes(product_params)
+      flash[:success] = "Product updated"
+      redirect_to products_path+"?grp_id=#{@product.group_id}"
+    else
+      render 'edit'
+    end
+  end
+  
+  def new
+    @product = Product.new
+    @product.is_leaf = params[:is_leaf]
+    @product.group_id = params[:grp_id]
+  end
+  
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      flash[:success] = "Product created"
+      redirect_to products_path+"?grp_id=#{@product.group_id}"
+    else
+      render 'new'
+    end
+  end
+  
+  private
+
+    def product_params
+      params.require(:product).permit(:name, :description, :measure, :is_service,
+                      :purchase_price, :cash_price, :noncash_price, :is_leaf, :group_id)
+    end
 
 end
--- spec/requests/product_pages_spec.rb
+++ spec/requests/product_pages_spec.rb
@@ -8,12 +8,89 @@ describe "Product pages" do
     let!(:product) { FactoryGirl.create(:product) }
     let!(:product_group) { FactoryGirl.create(:product_group) }
     let!(:product_in_group) { FactoryGirl.create(:product, group_id: product_group.id) }
+
+    describe "root path" do
+      before { visit products_path }
+
+      it { should have_content('Products list') }
+      it { should have_title('Products list') }
+      it { should have_content(product.name) }
+      it { should have_content(product_group.name) }
+      it { should_not have_content(product_in_group.name) }    
+    end
+
+    describe "subgroup path" do
+      before { visit products_path+"?grp_id=#{product_group.id}" }
+
+      it { should have_content(product_group.name) }
+      it { should have_content(product_in_group.name) }    
+    end
+  end #describe "index page"
+  
+  describe "profile page" do
+    let(:product) { FactoryGirl.create(:product) }
+    before { visit product_path(product) }
+
+    it { should have_title(product.name) }
+    it { should have_content(product.name) }
+  end #describe "profile page"
+
+  shared_examples_for "shared_with" do
+    before do
+      fill_in "Name", with: new_name
+      click_button "Update"
+    end
+
+    describe "invalid information" do
+      let(:new_name)  { "" }
+      it { should have_content('error') }
+    end
+
+    describe "valid information" do
+      let(:new_name)  { "New Product Name" }
+      it { should have_content(new_name) }
+      it { should have_selector('div.alert.alert-success') }
+    end
+  end
+
+  describe "edit" do
+    let(:product) { FactoryGirl.create(:product) }
+    let(:product_group) { FactoryGirl.create(:product_group) }
+    before { visit edit_product_path(product) }
+
+    describe "item" do
+      it { should have_title(product.name) }
+      it { should have_content(product.name) }
+    end
+
+    describe "group" do
+      before { visit edit_product_path(product_group) }
+      it { should_not have_content('price') }
+    end
+
+    describe "with_shared" do
+      it_should_behave_like "shared_with"
+    end
+  end #describe "edit"
+
+  describe "add" do
     before { visit products_path }
 
-    it { should have_content('Products list') }
-    it { should have_title('Products list') }
-    it { should have_content(product.name) }
-    it { should have_content(product_group.name) }
-    it { should_not have_content(product_in_group.name) }
-  end
+    describe "item" do
+      before { click_link "Add item" }
+      it { should have_title("New product") }
+      it { should have_content("New product") }
+    end
+
+    describe "group" do
+      before { click_link "Add group" }
+      it { should_not have_content('price') }
+    end
+
+    describe "with" do
+      before { click_link "Add item" }
+      it_should_behave_like "shared_with"
+    end
+  end #describe "add"
+
 end
