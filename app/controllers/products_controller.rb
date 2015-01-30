6df520c1520cf110fbf6fd730eb906742a56cf13
lemboy <lemboy@gmail.com>
2015-01-30 17:41:20 +0300

    Fix some bugs

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,7 +2,7 @@ class ProductsController < ApplicationController
 
   def index
     grp_id = params[:grp_id].blank? ? nil : params[:grp_id]
-    @products = Product.where(group_id: grp_id).order(:is_leaf, :name)
+    @products = Product.where(group_id: grp_id).order(is_leaf: :asc, name: :asc)
     @product_parents = Product.where("id in (?)", parents_list_id(grp_id)) unless grp_id.nil?
   end
   
